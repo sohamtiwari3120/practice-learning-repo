25c8f24baf9c5aa3133c84483c99319a623cb7f1
Soham Tiwari <soham.tiwari800@gmail.com>
2019-08-30 06:25:46 +0530

    Created first file, HelloWorld.c

---
+++ HelloWorld.c
@@ -0,0 +1,10 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+	printf("Enter the number below!\n");
+	int n;
+	scanf("%d",&n);
+	printf("The number entered is : %d\n", n+1);
+	return 0;
+}
